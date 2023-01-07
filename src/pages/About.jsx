f5b2fe241bf200b60a013601cbdcc3cdd46e7887
matiasdamian12 <matydamii@gmail.com>
2023-01-07 02:53:07 -0300

    frontfinal2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
     
     <div>
         <h1 className='text-center m-5'>Sobre nosotros</h1>
-        <h4 className='text-center m-6'>Somos una empresa dedicada a coseguirte y asesorarte un mejor seguro para tu vida</h4>
+        <h4 className='text-center m-6'>Somos una empresa dedicada a coseguirte y asesorarte un mejor seguro para tu vida y hogar</h4>
 
     </div>
     
